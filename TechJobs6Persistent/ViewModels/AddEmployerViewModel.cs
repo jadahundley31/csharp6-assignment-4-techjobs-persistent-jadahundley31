866b060cb989e7cb2a12ba733419800289cce2de
Jada <jada.hundley@health.slu.edu>
2023-12-03 15:56:50 -0600

    added employer view model validation

---
+++ TechJobs6Persistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechJobs6Persistent.ViewModels
+{
+	public class AddEmployerViewModel
+	{
+		[Required]
+		[StringLength(50)]
+		public string? Name { get; set; }
+
+		[Required]
+        [StringLength(50)]
+		public string? Location { get; set; }
+    }
+}
+
