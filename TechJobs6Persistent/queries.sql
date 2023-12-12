e4ebc32dd20b9089c6ca200fb1f61bc1128b7463
Jada <jada.hundley@health.slu.edu>
2023-12-11 22:35:34 -0600

    written queries

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -3,13 +3,18 @@
 
 
 --Part 1: List the columns and their data types in the Jobs table.
-    /* Columns:
+     Columns:
         Id: int AI PK
         Name: longtext
         EmployerId: int
-    */
+    
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
+    CREATE TABLE employers
+    name VARCHAR(255)
+    location VARCHAR(255)
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
+    UPDATE skills
+    SET name = ORDER BY NAME
