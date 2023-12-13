fbb6f912831440d6a79ef8b18756f5e0aac57545
Jada <jada.hundley@health.slu.edu>
2023-12-12 21:10:19 -0600

    updated queries

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -7,16 +7,16 @@
     
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
-    /*SELECT Employer.Name
+    /*SELECT Name
       FROM Employer
       WHERE Location = "St. Louis";
     */
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
-    /*SELECT DISTINCT Skills.SkillName 
+    /*SELECT DISTINCT SkillName 
       FROM Skills
       INNER JOIN JobSkills ON Skills.Id = JobSkills.SkillsId
       WHERE Skills.SkillName is not null
       ORDER BY Skills.SkillName ASC
-     */
+    */
